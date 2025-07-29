37a42003607ab8524a0c82b9c7330ab1895e3828
Ryan Rodrigues <ryanpimentelbr@gmail.com>
2025-07-28 22:42:05 -0300

    organizar menu principal e menus de objetos

--- projetoBancoDados/prompts.py
+++ projetoBancoDados/prompts.py
@@ -1,20 +1,31 @@
 from connection import *
 
 def menu():
+	print("Ola usuario!")
+	("Bem vindo ao banco de dados dos achados e perdidos")
 	while True:
-		print("ola usuario!")
-		print("bem vindo ao banco de dados dos achados e perdidos")
-		print("[1]adicionar pessoa")
-		print("[2]remover pessoa")
-		print("[3]sair")
-		opt=int(input("o que deseja fazer?"))
-		if opt==1:
-			prompt_add_pessoa()
-		if opt==2:
-			prompt_remove_pessoa()
-		if opt==3:
-			 break
+		print("\n--- ACHADOS E PERDIDOS ---")
+		print("[1] Pessoas")
+		print("[2] Objetos")
+		print("[3] Ocorrencias de perda")
+		print("[4] Reivindicações")
+		print("[5] sair")
+		opt = input("Qual registro deseja acessar? ")
+		if opt=="1":
+			menu_objetos()
+		elif opt=="2":
+			menu_objetos()
+		elif opt=="3":
+			menu_objetos()
+		elif opt=="4":
+			menu_objetos()
+		elif opt=="5":
+			print("Obrigado por usar o sistema de achados e perdidos")
+			break
+		else:
+			print("Opção inválida. Por favor, escolha uma opção válida.")
 
+# ------------------------------- MENUS pessoas -------------------------------
 
 def prompt_add_pessoa():
 	PessoaMatricula=input("qual a matricula da pessoa a ser adicionada? ")
@@ -30,12 +41,35 @@ def prompt_remove_pessoa():
 
 # ------------------------------- MENUS objetos -------------------------------
 
+# Menu de Opções de Objetos
+def menu_objetos():
+		while True:
+				print("\n--- OBJETOS ---")
+				print("[1] Adicionar Objeto Perdido")
+				print("[2] Remover Objeto Perdido")
+				print("[3] Editar Objeto Perdido")
+				print("[4] Listar Objetos Perdidos")
+				print("[5] Voltar ao Menu Principal")
+				opt = input("Escolha uma opção: ")
+				if opt == "1":
+						prompt_add_objeto()
+				elif opt == "2":
+						prompt_remove_objeto()
+				elif opt == "3":
+						prompt_edit_objeto()
+				elif opt == "4":
+						read_and_print_objetos()
+				elif opt == "5":
+						break
+				else:
+						print("Opção inválida. Por favor, escolha uma opção válida.")
+
 # Menu de adicionar objeto 
 def prompt_add_objeto():
     print("\n--- Adicionar Novo Objeto Perdido ---")
 
     # Campos obrigatórios
-    Otitulo = input("Qual o título/nome do objeto? (Ex: 'Chave', 'Celular', 'Mochila') ")
+    Otitulo = input("Qual o título/nome do objeto? ")
     Odescricao = input("Descreva o objeto detalhadamente: ")
     Olocal_encontrado = input("Onde o objeto foi encontrado? ")
 
@@ -74,12 +108,14 @@ def prompt_edit_objeto():
     try:
         objeto_id = int(input("Qual o ID do objeto a ser editado? "))
 
+        read_and_print_unico_objeto(objeto_id)
+
         print("\nEscolha o campo a ser editado:")
-        print("1. Título")
-        print("2. Cor")
-        print("3. Descrição")
-        print("4. Local Encontrado")
-        print("5. Pessoa que Entregou")
+        print("[1] Título")
+        print("[2] Cor")
+        print("[3] Descrição")
+        print("[4] Local Encontrado")
+        print("[5] Pessoa que Entregou")
 
         opcao = input("Digite o número da opção: ")
 
@@ -94,7 +130,7 @@ def prompt_edit_objeto():
         campo_a_editar = campos_menu.get(opcao)
 
         if not campo_a_editar:
-            print("Opção inválida. Por favor, digite um número de 1 a 5.")
+            print("Opção inválida. Por favor, escolha uma opção válida.")
             return
 
         novo_valor = input(f"Digite o novo valor para '{campo_a_editar}': ")
@@ -105,5 +141,5 @@ def prompt_edit_objeto():
         update_objeto_database(objeto_id, campo_a_editar, novo_valor)
         print("Transação de edição de objeto finalizada.")
     except ValueError:
-        print("Entrada inválida. Por favor, digite um número inteiro para o ID do objeto ou uma opção válida.")
+        print("Entrada inválida. Por favor, tente novamente.")
        
